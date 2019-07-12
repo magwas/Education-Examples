30dbe637638ed696d19171b1f138854baccf5214
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2019-07-12 11:07:28 +0200

    Fix assert JUnit version to 5

--- calculator/src/test/java/com/kodekonveyor/calculator/CalculatorTest.java
+++ calculator/src/test/java/com/kodekonveyor/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.kodekonveyor.calculator;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
